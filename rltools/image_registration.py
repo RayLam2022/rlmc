26ec7b0a4e8c44c0ebda36cd50726fff744f21e8
RayLam2022 <1027196450@qq.com>
2024-10-10 10:57:00 +0800

    image_registration

--- rltools/image_registration.py
+++ rltools/image_registration.py
@@ -3,10 +3,11 @@
 import numpy as np
 import cv2
 from PIL import ImageGrab
+from sklearn.cluster import DBSCAN
 
 
 def image_registration(
-    query_img_path: str, query_img_scale: float = 1.0, threshold: int = 460
+    query_img_path: str, query_img_scale: float = 1.0, threshold: float = 1.0, min_samples: int =4,is_show: bool = True
 ):
 
     # 读取拼接图片
@@ -29,7 +30,7 @@ def image_registration(
 
     # 创建匹配器
     bf = cv2.BFMatcher()
-    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)##
 
     # 应用比率测试
     good = []
@@ -38,27 +39,39 @@ def image_registration(
         if m.distance < 0.75 * n.distance:
             good.append(m)
             n_good.append(n)
-    if good:
-        pts2 = np.float32([kp2[n.trainIdx].pt for m in n_good]).reshape(-1, 1, 2)
-        mean_pt2 = np.mean(pts2, axis=0).ravel()
-
-        # 剔除明显超出平均范围的点
-        filtered_good = []
-        for m, n in zip(good, n_good):
-            pt = np.float32([kp2[n.trainIdx].pt])
-            distance = np.linalg.norm(pt - mean_pt2)
-            if distance < threshold:  # 这里的100是阈值，可以根据实际情况调整
-                filtered_good.append([m])
+    if not good:
+        print('没有匹配点')
+        return None
+    else:
         # 绘制图像
+        pts2 = np.float32([kp2[n.trainIdx].pt for n in n_good]).reshape(-1, 2)
+        dbscan = DBSCAN(eps=threshold, min_samples=min_samples)
+ 
+        # 对数据集进行聚类
+        labels = dbscan.fit_predict(pts2)
+        print('labels:',labels)
+        #is_core_sample = dbscan.core_sample_indices_
+        # 剔除离群点，保留标签为-1的点
+        # pts2 = pts2[~outliers.any(axis=1)]
+        filtered_good = []
+        for idx, (m, n) in enumerate(zip(good, n_good)):
+            if labels[idx]>=0:
+                filtered_good.append([m])
+
+
         img_matches = cv2.drawMatchesKnn(
             img1, kp1, img2, kp2, filtered_good, None, flags=2
         )
-        cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
-        cv2.imshow("Matches", img_matches)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if is_show:
+            cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
+            cv2.imshow("Matches", img_matches)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
-        if len(filtered_good) > 4:
+        if len(filtered_good) <= 4:
+            print('匹配点小于5')
+            return None
+        else:
             pts1 = np.float32([kp1[m[0].queryIdx].pt for m in filtered_good]).reshape(
                 -1, 1, 2
             )
@@ -89,23 +102,30 @@ def image_registration(
             img1_transformed = cv2.warpPerspective(img1, H, (new_width,new_height))
             #img1_transformed = cv2.resize(img1, (new_width, new_height))
             #print(img1_transformed.shape)
-            cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
-            cv2.imshow("Matches", img1_transformed)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            if is_show:
+                cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
+                cv2.imshow("Matches", img1_transformed)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
 
             # 将变换后的查询图像定位到全景图像中
-            img2[
-                int(min_y2 - min_y1) : int(min_y2 - min_y1) + new_height,
-                int(min_x2 - min_x1) : int(min_x2 - min_x1) + new_width,
-            ] = img1_transformed[:, :]
+            try:
+                img2[
+                    int(min_y2-min_y1*scale_y) : int(min_y2-min_y1* scale_y) + new_height,
+                    int(min_x2-min_x1*scale_x) : int(min_x2-min_x1*scale_x) + new_width,
+                ] = img1_transformed
+            except:
+                print("out of range，可能有异常点导致图片切片过大")
+                return None
 
             # 显示图像
-            cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
-            cv2.imshow("Matches", img2)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            else:
+                if is_show:
+                    cv2.namedWindow("Matches", cv2.WINDOW_NORMAL)
+                    cv2.imshow("Matches", img2)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
-    image_registration(query_img_path=r"D:\work\ComfyUI\output\ComfyUI_00070.png", query_img_scale=0.05)
+    image_registration(query_img_path=r"D:\work\ComfyUI\output\ComfyUI_00070.png", query_img_scale=0.2, threshold=22.0, min_samples=3, is_show=True)
